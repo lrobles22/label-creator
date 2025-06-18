b90b11ee193be08ff98eef9fe0536f937f2d30ec
Luis Robles <info@wtwheels.com>
2025-06-17 23:37:52 -0400

    ✅ Proyecto listo: cliente en index.html y admin en admin.html

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        admin: 'public/index.html',     // para /
-        client: 'public/client.html',   // para /client
-      },
-    },
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
-  publicDir: false,
+  plugins: [react()],
+  base: './',
 });

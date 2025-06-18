44e3a2d3c555949280d910c676c78d826ba33632
Luis Robles <info@wtwheels.com>
2025-06-17 23:58:56 -0400

    ✅ Fix: vite.config para local + producción

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-});
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),      // Cliente
+          admin: resolve(__dirname, 'admin.html'),     // Admin
+          client: resolve(__dirname, 'client.html')    // Cliente separado
+        }
+      }
+    },
+    root: command === 'serve' ? '.' : undefined // esto permite que `npm run dev` funcione normalmente
+  }
+})
